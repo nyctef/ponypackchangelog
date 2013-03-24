5ff5936c84f074eb1ab186052c22ad33b40b4c55
nyctef <nyctef@nyctef.com>
2013-03-24 23:24:31 +0000

    make debugging do something useful

--- ponypackchangelog/ponypackchangelog.py
+++ ponypackchangelog/ponypackchangelog.py
@@ -1,1 +1,14 @@
-print('Hello World')
+import argparse
+import unittest
+
+def run_tests():
+    unittest.main(module='test.compare_test', exit=False, argv=['ponypackchangelog.py', '-v'])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Do stuff')
+    parser.add_argument('-t', '--run-tests', action='store_true')
+    args = parser.parse_args()
+    if (args.run_tests):
+        run_tests()
+    
+    input('Press enter to finish ..')
